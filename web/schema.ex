70fd64c08b31a706d2fdd87af9a77154aaefdf7e
Victoria Romaniuk <victoria7maksimenko@gmail.com>
2019-08-28 15:45:46 +0300

    add mutations

--- web/schema.ex
+++ web/schema.ex
@@ -16,4 +16,33 @@ defmodule GraphqlAuth.Schema do
       resolve &GraphqlAuth.UserResolver.find/2
     end
   end
+
+  input_object :update_post_params do
+    field :title, non_null(:string)
+    field :body, non_null(:string)
+    field :user_id, non_null(:integer)
+  end
+
+  mutation do
+    field :create_post, type: :post do
+      arg :title, non_null(:string)
+      arg :body, non_null(:string)
+      arg :user_id, non_null(:integer)
+
+      resolve &GraphqlAuth.PostResolver.create/2
+    end
+
+    field :update_post, type: :post do
+      arg :id, non_null(:integer)
+      arg :post, :update_post_params
+
+      resolve &GraphqlAuth.PostResolver.update/2
+    end
+
+    field :delete_post, type: :post do
+      arg :id, non_null(:integer)
+
+      resolve &GraphqlAuth.PostResolver.delete/2
+    end
+  end
 end
